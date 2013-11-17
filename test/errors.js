16020c8ecdc9e305a4bfc60c8e1124f6df5e04ea
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-11-17 18:33:31 +0200

    Add one error test case

---
+++ test/errors.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+
+"use strict";
+
+var typify = require("../lib/typify.js");
+var assert = require("assert");
+
+describe("error cases", function () {
+  it("throws if errorneous type is given", function () {
+    assert.throws(function () {
+      typify.check("#foo", 1);
+    });
+  });
+});
