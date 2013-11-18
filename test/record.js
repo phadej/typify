205662bb0893fc3e5c4448ab66c43c1bd8c471d7
Oleg Grenrus <oleg.grenrus@iki.fi>
2013-11-18 14:56:58 +0200

    Anonymous records parser

--- test/record.js
+++ test/record.js
@@ -44,6 +44,42 @@ describe("records - record()", function () {
   });
 });
 
+describe("records - anonymous records", function () {
+  var typ;
+  var isPerson;
+
+  beforeEach(function () {
+    typ = typify.create();
+    typ.alias("person", "{name: string, age: number}");
+    isPerson = typ.check("person");
+  });
+
+  it("adds type definition which is falsy for non-objects", function () {
+    assert(!isPerson(undefined));
+    assert(!isPerson(null));
+    assert(!isPerson(true));
+    assert(!isPerson(false));
+    assert(!isPerson(0));
+    assert(!isPerson(1));
+    assert(!isPerson(""));
+    assert(!isPerson("foobar"));
+  });
+
+  it("is falsy for objects with missing properties", function () {
+    assert(!isPerson({}));
+    assert(!isPerson({ age: 10 }));
+    assert(!isPerson({ name: "foo" }));
+  });
+
+  it("is truthy for objects with all properties", function () {
+    assert(isPerson({ age: 10, name: "foo" }));
+  });
+
+  it("is truthy for objects with extra properties", function () {
+    assert(isPerson({ age: 10, name: "foo", height: 175 }));
+  });
+});
+
 describe("recursive records", function () {
   var typ;
 
